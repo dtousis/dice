8e0d1359b2702f9c67775b75f08d25d329cbd172
dtousis <98193216+dtousis@users.noreply.github.com>
2022-01-22 01:38:44 +0200

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,58 +7,58 @@ input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Square)
     αριθμός = randint(1, 6)
     if (αριθμός == 1) {
-        images.createImage(`
+        basic.showLeds(`
             . . . . .
             . . . . .
             . . # . .
             . . . . .
             . . . . .
-            `).showImage(0)
+            `)
     } else {
         if (αριθμός == 2) {
-            images.createImage(`
+            basic.showLeds(`
                 . . . . #
                 . . . . .
                 . . . . .
                 . . . . .
                 # . . . .
-                `).showImage(0)
+                `)
         } else {
             if (αριθμός == 3) {
-                images.createImage(`
+                basic.showLeds(`
                     . . . . #
                     . . . . .
                     . . # . .
                     . . . . .
                     # . . . .
-                    `).showImage(0)
+                    `)
             } else {
                 if (αριθμός == 4) {
-                    images.createImage(`
+                    basic.showLeds(`
                         # . . . #
                         . . . . .
                         . . . . .
                         . . . . .
                         # . . . #
-                        `).showImage(0)
+                        `)
                 } else {
                     if (αριθμός == 5) {
-                        images.createImage(`
+                        basic.showLeds(`
                             # . . . #
                             . . . . .
                             . . # . .
                             . . . . .
                             # . . . #
-                            `).showImage(0)
+                            `)
                     } else {
                         if (αριθμός == 6) {
-                            images.createImage(`
+                            basic.showLeds(`
                                 # . . . #
                                 . . . . .
                                 # . . . #
                                 . . . . .
                                 # . . . #
-                                `).showImage(0)
+                                `)
                         }
                     }
                 }
